329e7895acea04d4409c2d7fa36019de84e1ce74
moonlight0system <alexander.tschoep2510@gmail.com>
2022-05-04 21:36:02 +0200

    inizialize github repository

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js")
+
+const TOKEN = "ODkxMzkzMjI5NTQ1NzAxNDU2.YU9sow.O7ILbLwY6nvyETjXnOFI78oUsBo"
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == ".help"){
+        message.reply("Hi, how can I help you?")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == ".hug"){
+        message.reply("https://tenor.com/view/monsters-inc-boo-hug-big-hug-gif-24350322")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "Welcome back"){
+        message.reply("Welcome back to watch Mojo!")
+    }
+})
+
+client.login(TOKEN)
